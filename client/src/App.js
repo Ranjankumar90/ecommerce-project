1132182580986e77776659fc619ff694bd72cb97
ranjan kumar <ranjankrmth143@gmail.com>
2025-03-16 15:07:09 +0530

    Initial commit of e-commerce project

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/products')
+      .then((response) => setProducts(response.data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>E-Commerce Shopping</h1>
+      <div>
+        {products.map((product) => (
+          <div key={product.id}>
+            <h3>{product.name}</h3>
+            <p>${product.price}</p>
+            <p>{product.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
